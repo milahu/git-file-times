e6844af4a8cf80cc79b00387491bc8cd4fbf9807
Milan Hauth <milahu@milahu.duckdns.org>
2026-06-13 07:40:25 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -18,152 +18,15 @@ struct Context {
     std::unordered_set<std::string> remaining;
     std::unordered_set<std::string> ignore_next_add;
     std::unordered_map<std::string, time_t> result;
-    std::vector<std::string> to_remove;
     git_time_t time;
-    // const git_oid *oid; // debug
-    git_oid *oid; // debug
-    git_commit *commit; // debug
-
+    git_oid *oid;
+    git_commit *commit;
     // stats
     size_t commits = 0;
     size_t files_found = 0;
     std::chrono::steady_clock::time_point start_time;
     std::chrono::steady_clock::time_point last_print_time;
 };
-
-struct CommitCtx {
-    Context *ctx;
-    git_time_t time;
-    // const git_oid *oid; // debug
-    git_oid *oid; // debug
-    git_commit *commit; // debug
-    // std::vector<std::string> touched;
-    std::vector<std::string> to_remove;
-};
-
-#if false
-static int tree_cb(
-    const char *root,
-    const git_tree_entry *entry,
-    void *payload)
-{
-    auto *out = static_cast<std::unordered_set<std::string> *>(payload);
-
-    const char *name = git_tree_entry_name(entry);
-    if (!name) return 0;
-
-    std::string path = root ? std::string(root) + name : name;
-
-    if (git_tree_entry_type(entry) == GIT_OBJECT_BLOB) {
-        out->insert(path);
-    }
-
-    return 0;
-}
-#endif
-
-static inline std::string join_path(const char *root, const char *name)
-{
-    if (!root || root[0] == '\0')
-        return std::string(name);
-
-    std::string r(root);
-    if (r.back() != '/')
-        r += '/';
-
-    r += name;
-    return r;
-}
-
-static int tree_cb(
-    const char *root,
-    const git_tree_entry *entry,
-    void *payload)
-{
-    auto *ctx = static_cast<Context *>(payload);
-    // auto *ctx = static_cast<CommitCtx*>(payload);
-
-    if (git_tree_entry_type(entry) != GIT_OBJECT_BLOB) {
-        if (DEBUG) std::cerr << "no blob\n";
-        return 0;
-    }
-
-    const char *name = git_tree_entry_name(entry);
-    if (!name) {
-        if (DEBUG) std::cerr << "empty name\n";
-        return 0;
-    }
-
-    std::string path = join_path(root, name);
-
-    // debug
-    if (DEBUG) std::cerr << "path=" << path
-        << " commit=" << git_oid_tostr_s(ctx->oid)
-        // these are the same times
-        // << " ctx->commit->time=" << git_commit_time(ctx->commit)
-        // << " ctx->time=" << ctx->time
-        << " time=" << ctx->time
-        // << " status=" << delta->status
-        // << " old=" << (delta->old_file.path ?: "")
-        // << " new=" << (delta->new_file.path ?: "")
-        << "\n";
-
-    auto it = ctx->remaining.find(path);
-    if (it != ctx->remaining.end()) {
-        ctx->result[path] = ctx->time;
-        ctx->remaining.erase(it); // segfault
-        // ctx->to_remove.push_back(path);
-
-        if (ctx->remaining.empty())
-            return 1; // stop early
-    }
-
-    return 0;
-}
-
-// debug
-#if false
-static int tree_cb(
-    const char *root,
-    const git_tree_entry *entry,
-    void *payload)
-{
-    auto *ctx = (Context*)payload;
-
-    std::cerr << "CALLBACK\n";
-
-    return 0;
-}
-#endif
-
-#if false
-std::unordered_set<std::string> collect_head_tree(
-    git_repository *repo,
-    void *payload
-)
-{
-    std::unordered_set<std::string> out;
-
-    auto *ctx = static_cast<Context*>(payload);
-
-    git_object *obj = nullptr;
-    if (git_revparse_single(&obj, repo, "HEAD^{tree}") != 0 || !obj)
-        return out;
-
-    git_tree *tree = (git_tree *)obj;
-
-    // FIXME why not pass payload == ctx
-    git_tree_walk(tree, GIT_TREEWALK_PRE, tree_cb, &out);
-
-    // no!
-    // for (auto &p : ctx->to_remove)
-    //     ctx->remaining.erase(p);
-    // ctx->to_remove.clear();
-
-    git_tree_free(tree);
-    return out;
-}
-#endif
 
 static std::unordered_set<std::string> collect_head_tree(git_repository *repo)
 {
@@ -190,8 +53,8 @@ static std::unordered_set<std::string> collect_head_tree(git_repository *repo)
             const char *name = git_tree_entry_name(entry);
             if (!name) return 0;
 
+            // note: root ends with "/"
             std::string path = root && *root
-                // ? std::string(root) + "/" + name
                 ? std::string(root) + name
                 : name;
 
@@ -212,14 +75,6 @@ static int diff_cb(
 {
     auto *ctx = static_cast<Context *>(payload);
 
-    /*
-    const git_diff_file *file = &delta->new_file;
-    if (!file->path)
-        return 0;
-
-    std::string path(file->path);
-    */
-
     const git_diff_file *f = (
         delta->new_file.path ? &delta->new_file
         : &delta->old_file
@@ -227,8 +82,6 @@ static int diff_cb(
 
     if (!f->path) return 0;
 
-    // std::string path = f->path; // slow
-    // std::string_view path(f->path);
     const char *path = f->path;
 
     const git_diff_file *oldf = &delta->old_file;
@@ -241,9 +94,6 @@ static int diff_cb(
         // debug
         std::cerr << "path=" << path
             << " commit=" << git_oid_tostr_s(ctx->oid)
-            // these are the same times
-            // << " ctx->commit->time=" << git_commit_time(ctx->commit)
-            // << " ctx->time=" << ctx->time
             << " time=" << ctx->time
             << " status=" << delta->status
             << " old=" << (delta->old_file.path ?: "")
@@ -284,21 +134,6 @@ static int diff_cb(
     if (delta->status == GIT_DELTA_RENAMED)
         return 0;
 
-    // no, this is unreachable because of ctx->remaining.erase(path);
-    /*
-    if (
-        // path is part of the HEAD tree
-        ctx->remaining.count(path) &&
-        // path has been processed
-        ctx->seen.count(path)
-    ) {
-        std::cerr << "path2: " << path << "\n"; // debug
-    }
-    */
-
-    // no, this returns wrong file times
-    // because later commits can have more recent file times
-    // = because the first commit does not always have the latest file time
     if (
         // path is part of the HEAD tree
         ctx->remaining.count(path) &&
@@ -310,61 +145,8 @@ static int diff_cb(
         ctx->files_found++;
     }
 
-    /*
-    if (
-        // path is part of the HEAD tree
-        ctx->remaining.count(path)
-    ) {
-        // note: the default ctx->seen[path] is zero
-        // ctx->seen[path] = std::max(ctx->seen[path], ctx->time);
-        auto &val = ctx->seen[path];
-        val = (val == 0) ? ctx->time : std::max(val, ctx->time);
-    }
-    */
-
     return 0;
 }
-
-#if false
-static int diff_cb(
-    const git_diff_delta *delta,
-    float progress,
-    void *payload)
-{
-    auto *ctx = static_cast<Context *>(payload);
-
-    const char *path = nullptr;
-
-    switch (delta->status) {
-        case GIT_DELTA_ADDED:
-            path = delta->new_file.path;
-            break;
-
-        case GIT_DELTA_DELETED:
-            path = delta->old_file.path;
-            break;
-
-        case GIT_DELTA_MODIFIED:
-        case GIT_DELTA_RENAMED:
-        case GIT_DELTA_COPIED:
-            path = delta->new_file.path ? delta->new_file.path : delta->old_file.path;
-            break;
-
-        default:
-            return 0;
-    }
-
-    if (!path)
-        return 0;
-
-    if (ctx->remaining.count(path) && !ctx->seen.count(path)) {
-        ctx->seen[path] = ctx->time;
-        ctx->remaining.erase(path);
-    }
-
-    return 0;
-}
-#endif
 
 void print_stats(Context &ctx)
 {
@@ -412,37 +194,20 @@ int main() {
 
     git_revwalk_new(&walk, repo);
 
-    // no effect?
-    // git_revwalk_sorting(walk, GIT_SORT_TIME | GIT_SORT_TOPOLOGICAL);
-    // git_revwalk_sorting(walk, GIT_SORT_TIME);
-
-    // no, this returns empty output
-    // git_revwalk_hide_glob(walk, "*");
-
     git_revwalk_push_head(walk);
 
     git_revwalk_sorting(walk, GIT_SORT_TOPOLOGICAL);
 
-    // no, this makes it worse
-    // get only commits of the current branch
-    // git_revwalk_simplify_first_parent(walk);
-
-    // not working?
-    // skip merge commits
-    // git_revwalk_hide_glob(walk, "merge");
-
-    // Context ctx; // segfault
     auto ctx = std::make_unique<Context>();
 
     ctx->start_time = std::chrono::steady_clock::now();
     ctx->last_print_time = ctx->start_time;
 
-    // ctx->remaining = collect_head_tree(repo, &ctx);
-    // ctx->remaining = collect_head_tree(repo, ctx.get());
     ctx->remaining = collect_head_tree(repo);
 
     git_oid oid;
 
+    // loop commits
     while (!git_revwalk_next(&oid, walk)) {
 
         ctx->commits++;
@@ -452,8 +217,6 @@ int main() {
         }
 
         // std::cerr << "commit: " << git_oid_tostr_s(&oid) << "\n"; // debug
-
-        // auto ctx = std::make_unique<Context>(); / ???
 
         git_commit *commit;
         git_commit_lookup(&commit, repo, &oid);
@@ -476,50 +239,27 @@ int main() {
             git_commit_free(parent);
         }
 
-        // FIXME wrong time
         ctx->time = git_commit_time(commit);
-
-        ctx->oid = &oid; // debug
-        ctx->commit = commit; // debug
-
-
+        ctx->oid = &oid;
+        ctx->commit = commit;
 
         git_diff *diff = nullptr;
 
         git_diff_options opts = GIT_DIFF_OPTIONS_INIT;
         opts.flags |= GIT_DIFF_INCLUDE_UNTRACKED;
-        // opts.flags |= GIT_DIFF_RENAMES; // error: ‘GIT_DIFF_RENAMES’ was not declared
-        // opts.flags |= GIT_DIFF_FIND_RENAMES; // no, this makes it worse
 
         git_diff_tree_to_tree(&diff, repo, parent_tree, tree, &opts);
 
-        // TODO remove?
-        // no effect?
-        if (0) {
-            // detect file renames
-            git_diff_find_options find_opts = GIT_DIFF_FIND_OPTIONS_INIT;
-            find_opts.flags |= GIT_DIFF_FIND_RENAMES;
-            find_opts.flags |= GIT_DIFF_FIND_RENAMES_FROM_REWRITES;
-            find_opts.rename_threshold = 50; // optional tuning
-            git_diff_find_similar(diff, &find_opts);
-        }
-
+        // loop files
         git_diff_foreach(
             diff,
             diff_cb,
+            // TODO add binary_cb?
             nullptr,
             nullptr,
             nullptr,
-            // &ctx
             ctx.get()
         );
-
-        // no, this requires ctx->remaining.erase(path);
-        /*
-        if (ctx.remaining.empty())
-            break;
-        */
-
     }
 
     git_revwalk_free(walk);
