a002b657c9b0385ea065c311cc2ec19061c62fa0
Milan Hauth <milahu@milahu.duckdns.org>
2026-06-14 19:20:02 +0200

    fix debug-build.sh

--- debug-build.sh
+++ debug-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf build/; cmake -S . -B build-debug -DCMAKE_BUILD_TYPE=RelWithDebInfo && cmake --build build-debug -j1
+rm -rf debug-build; cmake -S . -B debug-build -DCMAKE_BUILD_TYPE=RelWithDebInfo && cmake --build debug-build -j1
